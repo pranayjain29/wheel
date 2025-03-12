ef27d9a819749ca9158569bc8eb0642ca02bfe03
pranayjain354 <pranayjain354@gmail.com>
2025-03-12 13:46:44 +0000

    V1.0

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+console.log("Inside supabasejs");
+
+import { createClient } from "/node-modules/@supabase/supabase-js"
+const supabase = createClient('https://dgjwvjcjdwdbgvnsznzn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnand2amNqZHdkYmd2bnN6bnpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3ODA3MDAsImV4cCI6MjA1NzM1NjcwMH0.EuP7eRQZqmdHS0nD_H04SQN2lsOSHxDh-hPP7n9QSkI')
+
+// console.log(supabase);
+// // const SUPABASE_URL = 'https://dgjwvjcjdwdbgvnsznzn.supabase.co';
+// // const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnand2amNqZHdkYmd2bnN6bnpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3ODA3MDAsImV4cCI6MjA1NzM1NjcwMH0.EuP7eRQZqmdHS0nD_H04SQN2lsOSHxDh-hPP7n9QSkI';
